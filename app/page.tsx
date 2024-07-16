c170a530fee18624b0025071ec1ace39f165553f
ichoukri <ichoukri1337@gmail.com>
2024-07-16 05:49:50 +0100

    add: Scatterplot chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { D3LineChart } from "@/components/charts/d3-line-chart";
+import { Scatterplot } from "@/components/charts/scatterplot";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <D3LineChart />
+      <Scatterplot />
     </main>
   );
 }
